094f7fa9dfe9ee0a1a6c9146fcc63b4e585e1454
William Weiss <william@codification.org>
2023-09-10 10:40:16 -0500

    feat: add new element size hook

---
+++ libs/react-element-size-hook/README.md
@@ -0,0 +1,7 @@
+# react-element-size-hook
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test react-element-size-hook` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ libs/react-element-size-hook/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'react-element-size-hook',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/react-element-size-hook',
+};
