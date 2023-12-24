9d98c7554efb98dc9f3338960578131078d4fd60
William Weiss <william@codification.org>
2023-12-23 19:39:35 -0800

    fix: possible window check fix

--- libs/react-element-size-hook/src/IsomorphicLayout.ts
+++ libs/react-element-size-hook/src/IsomorphicLayout.ts
@@ -1,4 +1,4 @@
 import { useEffect, useLayoutEffect as useWindowLayout } from 'react';
 
 export const useLayoutEffect =
-  window !== undefined ? useWindowLayout : useEffect;
+  typeof window === 'undefined' ? useWindowLayout : useEffect;
